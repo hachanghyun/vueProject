2140211d974c80f4856328dc373237c64bd9576d
hachanghyun <haccwx@naver.com>
2023-10-12 21:29:44 +0900

    예제 commit

--- vue-frontboard/src/views/board/BoardList.vue
+++ vue-frontboard/src/views/board/BoardList.vue
@@ -1,47 +1,4 @@
-<template>
-  <div class="board-list">
-    <div class="common-buttons">
-      <button type="button" class="w3-button w3-round w3-blue-gray" v-on:click="fnWrite">등록</button>
-    </div>
-    <table class="w3-table-all">
-      <thead>
-      <tr>
-        <th>No</th>
-        <th>제목</th>
-        <th>작성자</th>
-        <th>등록일시</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(row, idx) in list" :key="idx">
-        <td>{{ row.idx }}</td>
-        <td><a v-on:click="fnView(`${row.idx}`)">{{ row.title }}</a></td>
-        <td>{{ row.author }}</td>
-        <td>{{ row.created_at }}</td>
-      </tr>
-      </tbody>
-    </table>
-    <div class="pagination w3-bar w3-padding-16 w3-small" v-if="paging.total_list_cnt > 0">
-      <span class="pg">
-      <a href="javascript:;" @click="fnPage(1)" class="first w3-button w3-bar-item w3-border">&lt;&lt;</a>
-      <a href="javascript:;" v-if="paging.start_page > 10" @click="fnPage(`${paging.start_page-1}`)"
-         class="prev w3-button w3-bar-item w3-border">&lt;</a>
-      <template v-for=" (n,index) in paginavigation()">
-          <template v-if="paging.page==n">
-              <strong class="w3-button w3-bar-item w3-border w3-green" :key="index">{{ n }}</strong>
-          </template>
-          <template v-else>
-              <a class="w3-button w3-bar-item w3-border" href="javascript:;" @click="fnPage(`${n}`)" :key="index">{{ n }}</a>
-          </template>
-      </template>
-      <a href="javascript:;" v-if="paging.total_page_cnt > paging.end_page"
-         @click="fnPage(`${paging.end_page+1}`)" class="next w3-button w3-bar-item w3-border">&gt;</a>
-      <a href="javascript:;" @click="fnPage(`${paging.total_page_cnt}`)" class="last w3-button w3-bar-item w3-border">&gt;&gt;</a>
-      </span>
-    </div>
-  </div>
-</template>
-
+/* views/BoardList.vue */
 <script>
 export default {
   data() { //변수생성
@@ -79,7 +36,7 @@ export default {
   },
   methods: {
     fnGetList() {
-      this.requestBody = { // 데이터 전송
+      this.requestBody = { // 데이터 전송        
         keyword: this.keyword,
         page: this.page,
         size: this.size
@@ -97,6 +54,24 @@ export default {
           alert('네트워크가 원활하지 않습니다.\n잠시 후 다시 시도해주세요.')
         }
       })
+    },
+    fnView(idx) {
+      this.requestBody.idx = idx
+      this.$router.push({
+        path: './detail',
+        query: this.requestBody
+      })
+    },
+    fnWrite() {
+      this.$router.push({
+        path: './write'
+      })
+    },
+    fnPage(n) {
+      if (this.page !== n) {
+        this.page = n
+        this.fnGetList()
+      }
     }
   }
 }
